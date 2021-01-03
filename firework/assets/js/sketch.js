f558fa8e9f964f2467b4b612e4bb62e17a010243
master <jellemax08@hotmail.com>
2021-01-03 23:14:40 +0100

    normal fireworks

---
+++ firework/assets/js/sketch.js
@@ -0,0 +1,22 @@
+let fireworks = [];
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    background(0);
+    gravity = createVector(0, 0.2);
+}
+
+function draw() {
+    background(0, 25);
+
+    if(random() < 0.02){
+        fireworks.push(new Firework());
+    }
+    for (let i = fireworks.length - 1; i >= 0; i--) {
+        fireworks[i].update();
+        fireworks[i].show();
+        if (fireworks[i].done()) {
+            fireworks .splice(i, 1);
+        }
+    }
+}
