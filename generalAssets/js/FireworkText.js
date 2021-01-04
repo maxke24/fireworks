f67029a95d89d6939d4218348c03f1e807bef2f9
jelle <jellemax08@hotmail.com>
2021-01-04 14:19:08 +0100

    using inheritance for FireWork.js

--- generalAssets/js/FireworkText.js
+++ generalAssets/js/FireworkText.js
@@ -1,8 +1,6 @@
-class FireworkText {
+class FireworkText extends Firework{
     constructor(text, section) {
-        this.color = [random(150, 255), random(150, 255), random(150, 255)];
-        this.exploded = false;
-        this.particles = [];
+        super();
         let bounds = font.textBounds(text);
         switch (section) {
             case 0:
@@ -18,32 +16,7 @@ class FireworkText {
                 this.firework = new ParticleText(random(bounds.w/2, width - bounds.w / 2), height, this.color, createVector(), true);
                 break;
         }
-
         this.text = text;
-    }
-
-    done() {
-        return this.exploded && this.particles.length === 0;
-    }
-
-    update() {
-        if (!this.exploded) {
-            this.firework.applyForce(gravity);
-            this.firework.update();
-
-            if (this.firework.vel.y >= 0) {
-                this.exploded = true;
-                this.explode();
-            }
-        }
-
-        for (let i = this.particles.length - 1; i >= 0; i--) {
-            this.particles[i].applyForce(gravity);
-            this.particles[i].update();
-            if (this.particles[i].done()) {
-                this.particles.splice(i, 1);
-            }
-        }
     }
 
     explode() {
@@ -53,14 +26,5 @@ class FireworkText {
             let p = new ParticleText(this.firework.pos.x, this.firework.pos.y, this.color, textPoints[i]);
             this.particles.push(p);
         }
-    }
-
-    show() {
-        if (!this.exploded) {
-            this.firework.show();
-        }
-        this.particles.forEach(particle => {
-            particle.show();
-        })
     }
 }
